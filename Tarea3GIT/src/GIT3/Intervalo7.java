0bcaceb1b094b101c8333387dfcf4ed55b492a05
David <a_david.flores.gutierrez@iespablopicasso.es>
2023-11-27 09:15:23 +0100

    Programa intervalo

---
+++ Tarea3GIT/src/GIT3/Intervalo7.java
@@ -0,0 +1,53 @@
+package GIT3;
+import java.util.Scanner;
+public class Intervalo7 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		// Solicitamos los números
+
+        System.out.print("Introduce el primer número entero: ");
+
+        int num1 = scanner.nextInt();
+
+        
+
+        System.out.print("Introduce el segundo número entero (distinto al primero): ");
+
+        int num2 = scanner.nextInt();
+
+        
+
+        // comprobar que los números sean distintos
+
+        while (num1 == num2) {
+
+        	System.out.println("Los números deben ser distintos. Introduce el segundo número de nuevo: ");
+            num2 = scanner.nextInt();
+        }
+        // Determinar cuál es el menor de los números introducidos
+
+        int menor = Math.min(num1, num2);
+
+        int mayor = Math.max(num1, num2);
+
+        
+
+        System.out.println("Los números enteros entre " + menor + " y " + mayor + ", incrementando de 7 en 7 son:");
+
+        
+
+        // Imprimir los números entre el menor y el mayor, incrementando de 7 en 7
+
+        for (int i = menor; i <= mayor; i += 7) {
+
+            System.out.println(i);
+
+        }
+
+        scanner.close();
+    }	
+
+	}
+
+
