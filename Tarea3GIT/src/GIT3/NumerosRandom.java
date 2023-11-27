6bd51628a7d62f717a8943ba4e68f6d3de9a5f0d
David <a_david.flores.gutierrez@iespablopicasso.es>
2023-11-27 08:54:39 +0100

    Módificación 30 números

--- Tarea3GIT/src/GIT3/NumerosRandom.java
+++ Tarea3GIT/src/GIT3/NumerosRandom.java
@@ -6,8 +6,8 @@ public class NumerosRandom {
 	public static void main(String[] args) {
 		Scanner sc  = new Scanner (System.in);
 		Random rand = new Random();
-		System.out.println("20 numeros random entre 1 y 10.");
-		for (int  i = 0; i <20; i++ ) {
+		System.out.println("30 numeros random entre 1 y 10.");
+		for (int  i = 0; i <30; i++ ) {
 			int numeroRandom = rand.nextInt(10) +1;
 			System.out.println(numeroRandom + " ");
 		}
